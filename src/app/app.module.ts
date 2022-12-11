f84c59f9cedc68b7c9baa1ec12f2c3a7128fefc4
Karim Mamdouh Mohamed <ghost_594@hotmail.com>
2022-12-11 20:11:07 +0200

    Inital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  CICButtonModule,
+  CICDataTableModule,
+  CICInputModule,
+  CICCheckboxModule,
+  CICDividerModule,
+  CICDropdownModule,
+  CICFieldSetModule,
+  CICImageModule,
+  CICKnobModule,
+  CICRadioButtonModule,
+  CICRatingModule,
+  CICSliderModule,
+} from 'cic-package-test';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CICButtonModule,
+    CICInputModule,
+    CICDataTableModule,
+    CICCheckboxModule,
+    CICDividerModule,
+    CICDropdownModule,
+    CICFieldSetModule,
+    CICImageModule,
+    CICKnobModule,
+    CICRadioButtonModule,
+    CICRatingModule,
+    CICSliderModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
